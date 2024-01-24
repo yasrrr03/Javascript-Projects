4464b1bd309bdce78194dbd98baede38dd7d92e5
yasrrr03 <124323936+yasrrr03@users.noreply.github.com>
2024-01-24 18:11:36 +0000

    Update slot-machine.js

--- slot-machine.js
+++ slot-machine.js
@@ -1,11 +1,3 @@
-// 1. Deposit some money '
-// 2. Determine number of lines to bet on '
-// 3. Collect a bet amount '
-// 4. Spin the slot machine '
-// 5. Check if user won  
-// 6. Give winnings
-// 7. Play again
-
 const prompt = require("prompt-sync")();
 const ROWS = 3;
 const COLS = 3;
@@ -24,6 +16,7 @@ const SYMBOLS_VALUES = {
     "D": 2
 }
 
+// 1. Deposit some money 
 const deposit = () => {
 while (true) {
     const depositAmount = prompt("Enter a deposit amount: ");
@@ -37,6 +30,7 @@ while (true) {
 }
 };
 
+// 2. Determine number of lines to bet on 
 const getNumberOfLines = () => {
     while (true) {
         const lines = prompt("Enter the number of lines to bet on (1-3): ");
@@ -50,6 +44,7 @@ const getNumberOfLines = () => {
     }
 } 
 
+// 3. Collect a bet amount 
 const getBet = (balance, lines) => {
     while (true) {
         const bet = prompt("Enter the bet per line: ");
@@ -62,6 +57,8 @@ const getBet = (balance, lines) => {
         }
     } 
 }
+
+// 4. Spin the slot machine 
 const spin = () => {
     const symbols = [];
     for (const[symbol, count] of Object.entries(SYMBOLS_COUNT)) {
@@ -99,6 +96,7 @@ const transpose = (reels) => {
     return rows
 } 
 
+// 5. Check if user won  
 const printRows = (rows) => {
     for (const row of rows) {
         let rowString = "";
@@ -113,6 +111,7 @@ const printRows = (rows) => {
     }
 }
 
+// 6. Give winnings
 const getWinnings = (rows, bet, lines) => {
     let winnings = 0;
 
@@ -155,6 +154,7 @@ const game = () => {
             break
         }
 
+// 7. Play again
         const playAgain = prompt("Do you want to play again (y/n)? ")
 
         if (playAgain != "y") break;
